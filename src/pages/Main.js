ef1070dc8eb6715843a8831948c2f8f9b377f820
yoodeve <janhold@naver.com>
2022-06-24 17:53:50 +0900

    모리겟따!

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -2,8 +2,13 @@ import React from "react";
 import styled from "styled-components";
 import Gradient from "../element/Gradient";
 
+import '../css/main.css';
+
 import back from "../image/back.png";
 import logo from "../image/smallLogo.png";
+import intro from "../image/IntroWall.png";
+import port from '../image/PortWall.png';
+import req from '../image/ReqWall.png';
 
 import SideBar from "../component/SideBar";
 import Navigation from "../component/Navigation";
@@ -11,65 +16,68 @@ import Navigation from "../component/Navigation";
 const Main = (toggleMenu) => {
   return (
     <>
-    <SideBar />
-      <MainWrap>
-        <TopGrad>
-          <LogoDiv src={logo} />
+      <SideBar />
+      <div className="main-wrap">
+        <div className="top-grad">
+          <div className="logo-div" src={logo} />
           <Navigation toggleMenu={toggleMenu} absolute />
-        </TopGrad>
-
-        <IMG src={back} />
+        </div>
+        <img className="bckgrnd" src={back} />
         <Gradient
-        position="absolute"
+          position="absolute"
           margin="auto"
           bottom="0"
           height="296px"
           beginLinear="0%"
           endLinear="100%"
         />
-      </MainWrap>
+      </div>
     </>
   );
 };
 
-const MainWrap = styled.div`
-  width: 100vw;
-  height: 100vh;
-`;
+// const MainWrap = styled.div`
+//   width: 100vw;
+//   height: 100vh;
+//   @media (max-width: 1024px) {
+//     width: auto;
+//     height: 100vh;
+//   }
+// `;
 
-const TopGrad = styled.div`
-  width: 100vw;
-  height: 296px;
-  /* position: absolute; */
-  top: 0;
-  background: linear-gradient(0deg, rgba(0, 0, 0, 0) 0%, #000000 100%);
-`;
+// const TopGrad = styled.div`
+//   width: 100vw;
+//   height: 296px;
+//   /* position: absolute; */
+//   /* top: 0; */
+//   background: linear-gradient(0deg, rgba(0, 0, 0, 0) 0%, #000000 100%);
+// `;
 
-const LogoDiv = styled.div`
-  height: 38px;
-  width: 150px;
-  margin: 20px;
-  background-image: url("${(props) => props.src}");
-  z-index: 1;
-  position: absolute;
-  top: 0;
-`;
+// const LogoDiv = styled.div`
+//   height: 38px;
+//   width: 150px;
+//   margin: 20px;
+//   background-image: url("${(props) => props.src}");
+//   z-index: 1;
+//   position: absolute;
+//   top: 0;
+// `;
 
-const IMG = styled.img`
-  z-index: -1;
-  display: flex;
-  background-image: url("${(props) => props.src}");
-  /* width: 100vw; */
-  /* height: 100vh; */
-  position: fixed;
-  top: 0;
-  background-repeat: no-repeat;
-`;
+// const IMG = styled.img`
+//   z-index: -1;
+//   display: flex;
+//   background-image: url("${(props) => props.src}");
+//   /* width: 100vw; */
+//   /* height: 100vh; */
+//   position: fixed;
+//   top: 0;
+//   background-repeat: no-repeat;
+// `;
 
-const NavWrapper = styled.div`
-  width: 100vw;
-  bottom: 0;
-  position: fixed;
-`;
+// const NavWrapper = styled.div`
+//   width: 100vw;
+//   bottom: 0;
+//   position: fixed;
+// `;
 
 export default Main;
